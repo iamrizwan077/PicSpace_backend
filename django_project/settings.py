622db2a54a9927ab0b629f83867d738a6deba768
iamrizwan077 <iamrizwan077@gmail.com>
2022-11-22 08:18:12 +0500

    Shifted database from Postgres to MySql

--- django_project/settings.py
+++ django_project/settings.py
@@ -122,7 +122,16 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iamrizwan$memosdatabase',
+        'USER': 'iamrizwan',
+        'PASSWORD': 'memosdatabase',
+        'HOST': 'iamrizwan.mysql.pythonanywhere-services.com',
+    }
+}
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -133,7 +142,6 @@ DATABASES = {
         'PORT': 5432
     }
 }
-"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
